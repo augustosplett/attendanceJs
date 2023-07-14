605d7224c3dab7857ee62f6af36b1a3d9c397459
Augusto Madeira Splett (Tinho) <31394293+augustosplett@users.noreply.github.com>
2023-07-14 11:27:29 -0300

    FIX: fixed the way the name of the students are listed on the page

--- assets/scripts/students.js
+++ assets/scripts/students.js
@@ -1,16 +1,27 @@
 async function getStudentsList(){
-    console.log("chamou api")
     data = fetch('http://localhost:5000/student')
     .then(request => request.json())
     return data;
 }
 
+function clearList(list) {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+  }
+
 async function updateStudentsList(){
+    //defining the element tha will receive the students name
     listOfStudents = document.getElementById('students-list')
 
+    //clearing this list befor including new data to avoid duplication
+    clearList(listOfStudents)
+
+    //getting the list of students
     data = await getStudentsList()
-    data.forEach(el => {
-        console.log(el[1])
+
+    //creating the elements tha will be inserted on the list
+    data.forEach((el, index) => {
         const line = document.createElement('li');
         line.className = 'list-group-item';
 
@@ -18,12 +29,12 @@ async function updateStudentsList(){
         lineRadio.type = 'radio';
         lineRadio.name = 'student-group';
         lineRadio.className = 'form-check-input me-1';
-        lineRadio.id = el[0];
+        lineRadio.id = index;
 
         const lineText = document.createElement('label') ;
         lineText.className = 'form-check-label';
-        lineText.for = el[0];
-        lineText.innerHTML = `${el[1]} ${el[2]}`;
+        lineText.for = index;
+        lineText.innerHTML = `${el.first_name} ${el.last_name}`;
 
         line.appendChild(lineRadio);
         line.appendChild(lineText);
